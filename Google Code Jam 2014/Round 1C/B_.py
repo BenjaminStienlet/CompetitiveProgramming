8ffbf268e0a8f67e79f5c5b6dc723a05944642af
Benjamin Stienlet <bstienlet@gmail.com>
2014-05-10 10:56:20 +0100

    round 1c

---
+++ Google Code Jam 2014/Round 1C/B_.py
@@ -0,0 +1,22 @@
+__author__ = 'Benjamin S.'
+
+
+fi = open('input.txt', 'r')
+fo = open('output.txt', 'w')
+
+
+def read_line():
+    return fi.readline()
+
+
+def write_line(line):
+    fo.write(line + "\n")
+
+
+def output():
+    return None
+
+
+C = int(read_line())
+for c in range(C):
+    write_line("Case #%s: %s" % (c+1, output()))
