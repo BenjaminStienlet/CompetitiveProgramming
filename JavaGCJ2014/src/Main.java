d2b0d11e045a50a6cc0eb6b9275030b3d0b75951
Benjamin Stienlet <bstienlet@gmail.com>
2015-03-14 20:40:43 +0100

    Competitive programmin java template

---
+++ JavaGCJ2014/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new Main();
+    }
+
+    public Main() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+
+        }
+        out.flush();
+        out.close();
+        sc.close();
+    }
+}
